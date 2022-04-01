ece47a23a7c1b4cde4cd673c0b2fc11e6cce7ddc
Justus Kiplimo <59371869+jk45limo@users.noreply.github.com>
2022-04-01 14:06:30 +0300

    Edit on the front-end

---
+++ mern_exercise_tracker/src/components/create-exercise.component.js
@@ -0,0 +1,10 @@
+import React,{ Component }from 'react';
+export default class CreateExercise extends Component {
+render() {
+  return (
+    <div>
+      <p>You are on the Create Exercises List component!</p>
+    </div>
+  )
+}
+}
